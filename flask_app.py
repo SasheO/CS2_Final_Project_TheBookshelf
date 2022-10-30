c037f5eb6967aaba2a50fe7632790d49c1e0fa84
SasheO <76496417+SasheO@users.noreply.github.com>
2022-10-30 19:46:03 -0400

    partly populated functions for home and login routes

--- flask_app.py
+++ flask_app.py
@@ -3,16 +3,22 @@ from collections import defaultdict
 import json
 
 app = Flask(__name__)
-app.data = defaultdict(int)
 
 @app.route("/", methods=['GET'])
 @app.route("/home", methods=['GET'])
 def home():
-  # todo: ask user to log in or sign up
-  return "<p>Welcome to The Bookshelf!</p>"
+  return "Hello bookworm!\n Enter (1) to Login or (2) Sign Up to access the Bookshelf..."
+  
 
 @app.route("/login", methods=['POST'])
 def login():
+  data = json.loads(request.data)
+  # data should contain username and password. if it doesn't, ask them to log in.
+  if "username" not in data:
+    return "Invalid login details"
+  if "password" not in data:
+    return "Invalid login details"
+  # todo: verify username in database, if not, ask them to sign in
   # todo: check if user has an account with those credentials in database
   pass
 
