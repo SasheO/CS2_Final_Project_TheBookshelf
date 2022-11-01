651ebb7fcb2ed36a5ec573135d7b2a1da95c07f6
SasheO <76496417+SasheO@users.noreply.github.com>
2022-10-31 22:05:46 -0400

    updated signup function in flask_app.py, fixed errors in book and user types
    need to fix internal server error which occurs when you make request to signup route

--- flask_app.py
+++ flask_app.py
@@ -1,7 +1,11 @@
 from flask import Flask, request, jsonify
-import json
+import json, pickle
+import book
+from user import User
 
 app = Flask(__name__)
+
+users = []
 
 @app.route("/", methods=['GET'])
 @app.route("/home", methods=['GET'])
@@ -26,10 +30,35 @@ def login():
 def signup():
   response = {'msg': ""}
   data = json.loads(request.data)
+  print(data)
   if "username" not in data:
     response['msg'] = "Enter username"
+    return jsonify(response)
   if "password" not in data:
     response['msg'] = "Enter password"
-  # todo: verify that username is not take
-  # todo: save user to file using pickl?  
+    return jsonify(response)
+  username = data["username"]
+  password = data["password"]
+
+  # verify that username is not taken
+  file = open('users.pkl','rb')
+  users = pickle.load(file) # will be load a list of already existing User type objects
+  file.close()
+  
+  for user in users:
+    if user.username == username:
+      response['msg'] = "username taken, choose another one"
+      return jsonify(response)
+  
+  # todo: verify password appropriate complexity
+
+  new_user = User(username, password)
+  users.append(new_user)
+  # Open a file to write bytes
+  p_file = open('users.pkl', 'wb')
+  # Pickle the list
+  pickle.dump(users, p_file)
+  p_file.close()
+  response['msg'] = f"welcome to the Bookshelf {username}"
   return jsonify(response)
+  
