1d40cf6835e3a48ef4760800c8d3308b001d6434
SasheO <76496417+SasheO@users.noreply.github.com>
2022-11-09 11:27:55 -0500

    updated book objects to have available_for_lending variable

--- book.py
+++ book.py
@@ -3,8 +3,15 @@ class Book:
         self.title = title
         self.author = author
         self.genre = genre
+        self.available_for_lending = True
         # todo: should also save in server
     
+    def lend(self):
+        self.available_for_lending = False
+
+    def unlend(self):
+        self.available_for_lending = True
+
     def __del__(self):
         # todo: should also delete in server
         pass 
